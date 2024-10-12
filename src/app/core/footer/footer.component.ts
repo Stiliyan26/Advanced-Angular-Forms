674f1b9d0e234c343ad3191839d8933027f09efa
Stiliyan26 <stili262002@abv.bg>
2024-10-12 16:49:30 +0300

    Form options

---
+++ src/app/core/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FooterComponent {
+
+  get year() {
+    return new Date().getFullYear();
+  }
+}
---
+++ src/app/playgrounds/template-forms/template-forms-page/template-forms-page.component.ts
@@ -0,0 +1,41 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UserInfo } from '../../../core/models/user-info';
+
+@Component({
+  selector: 'app-template-forms-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './template-forms-page.component.html',
+  styleUrls: [
+    '../../common-page.scss',
+    '../../common-form.scss',
+    './template-forms-page.component.scss'
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TemplateFormsPageComponent implements OnInit {
+
+  userInfo: UserInfo = {
+    firstName: '',
+    lastName: '',
+    nickname: '',
+    email: '',
+    yearOfBirth: 0,
+    passport: '',
+    fullAddress: '',
+    city: '',
+    postCode: 0
+  };
+
+  constructor() { }
+
+  get years() {
+    const now = new Date().getUTCFullYear();
+    return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+  }
+
+  ngOnInit(): void {
+  }
+}
