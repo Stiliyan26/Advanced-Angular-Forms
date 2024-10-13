de947548c27da6c8705c3de6635d8bcdd4c78b44
Stiliyan26 <stili262002@abv.bg>
2024-10-13 16:47:08 +0300

    Optimzie validator for a one word at a time

---
+++ src/app/playgrounds/template-forms/validators/ban-words.directive.ts
@@ -0,0 +1,36 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+@Directive({
+  selector: '[appBanWords]',
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: BanWordsDirective,
+      multi: true
+    }
+  ]
+})
+export class BanWordsDirective implements Validator {
+
+  @Input()
+  set appBanWords(value: string | string[]) {
+    this.bannedWords = Array.isArray(value)
+      ? value
+      : [value];
+  };
+
+  private bannedWords: string[] = [];
+
+  constructor() { }
+
+  validate(control: AbstractControl<string>): ValidationErrors | null {
+    const foundBannedWords: string | undefined = this.bannedWords
+      .find((bannedWord: string) => bannedWord.toLowerCase() === control.value.toLowerCase())
+
+    return !foundBannedWords
+      ? { appBanWords: { bannedWords: foundBannedWords } }
+      : null;
+  }
+}
