7a64397d51d2079d70a8740f8747e3b7766b4c63
Stiliyan26 <stili262002@abv.bg>
2024-10-19 17:47:39 +0300

    Reduce boilerplate using Form Builders

--- src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
+++ src/app/playgrounds/reactive-forms/reactive-forms-page/reactive-forms-page.component.ts
@@ -1,6 +1,6 @@
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Observable, tap } from 'rxjs';
 import { UserSkillsService } from '../../../core/user-skills.service';
 
@@ -21,68 +21,48 @@ export class ReactiveFormsPageComponent implements OnInit {
   phoneLabels = ['Main', 'Mobile', 'Work', 'Home'];
   years = this.getYears();
   skills$!: Observable<string[]>;
-  //It is used to set a diffrent types
-  // UntypeFormControl
 
-  //When resetting the filed it will set the default value otherwise it will be empty
-  // firstName: new FormControl<string>('Dmytro', { nonNullable: true }),
+  private fb = inject(FormBuilder);
+  private userSkills = inject(UserSkillsService);
 
-  form = new FormGroup({
-    firstName: new FormControl<string>('Dmytro'),
-    lastName: new FormControl('Mezhenskyi'),
-    nickname: new FormControl(''),
-    email: new FormControl('dmytro@decodedfrontend.io'),
-    yearOfBirth: new FormControl(this.years[this.years.length - 1], { nonNullable: true }),
-    passport: new FormControl(''),
-    address: new FormGroup({
-      fullAddress: new FormControl('', { nonNullable: true }),
-      city: new FormControl('', { nonNullable: true }),
-      postCode: new FormControl(0, { nonNullable: true }),
+  form = this.fb.group({
+    firstName: 'Dmytro',
+    lastName: 'Mezhenskyi',
+    nickname: '',
+    email: 'dmytro@decodedfrontend.io',
+    yearOfBirth: this.fb.nonNullable.control(this.years[this.years.length - 1]),
+    passport: '',
+    address: this.fb.nonNullable.group({
+      fullAddress: '',
+      city: '',
+      postCode: 0,
     }),
-    phones: new FormArray([
+    phones: this.fb.array([
+      this.fb.group({
+        label: this.fb.nonNullable.control(this.phoneLabels[0]),
+        phone: ''
+      })
+    ]),
+    skills: this.fb.record<boolean>({})
+  });
+
+  ngOnInit(): void {
+    this.skills$ = this.userSkills.getSkills().pipe(
+      tap(skills => this.buildSkillControls(skills))
+    );
+  }
+
+  addPhone() {
+    this.form.controls.phones.insert(0,
       new FormGroup({
         label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
         phone: new FormControl('')
       })
-    ]),
-    skills: new FormGroup<{ [key: string]: FormControl<boolean> }>({})
-  });
-
-
-  constructor(private userSkills: UserSkillsService) { }
-
-  ngOnInit(): void {
-    this.skills$ = this.userSkills.getSkills()
-      .pipe(
-        tap(skills => this.buildSkillControls(skills))
-      );
-  }
-
-  buildSkillControls(skills: string[]): void {
-    skills.forEach(skill => {
-      this.form.controls.skills.addControl(
-        skill,
-        new FormControl(false, { nonNullable: true })
-      )
-    });
-  }
-
-  addPhone() {
-    // this.form.controls.phones.push(new FormControl(''));
-
-    //Specifies in wich index should the form control be added
-    this.form.controls.phones.insert(0, this.addNewPhoneFormGroup());
+    )
   }
 
   removePhone(index: number) {
     this.form.controls.phones.removeAt(index);
-  }
-
-  private addNewPhoneFormGroup() {
-    return new FormGroup({
-      label: new FormControl(this.phoneLabels[0], { nonNullable: true }),
-      phone: new FormControl('')
-    });
   }
 
   onSubmit(e: Event) {
@@ -91,6 +71,16 @@ export class ReactiveFormsPageComponent implements OnInit {
 
   private getYears() {
     const now = new Date().getUTCFullYear();
+    
     return Array(now - (now - 40)).fill('').map((_, idx) => now - idx);
+  }
+
+  private buildSkillControls(skills: string[]) {
+    skills.forEach(skill =>
+      this.form.controls.skills.addControl(
+        skill,
+        new FormControl(false, { nonNullable: true })
+      )
+    );
   }
 }
